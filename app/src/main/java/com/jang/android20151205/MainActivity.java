8dcf2ed1a64e51a5c9fe2f758c178cdd5e4910bb
janghe11 <jang0913@gmail.com>
2015-12-11 23:43:57 +0900

    commit 20151211

--- app/src/main/java/com/jang/android20151205/MainActivity.java
+++ app/src/main/java/com/jang/android20151205/MainActivity.java
@@ -1,13 +1,18 @@
 package com.jang.android20151205;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TITLE BAR 지우기 (초기화)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // 화면에 출력될 View 선택
         setContentView(R.layout.activity_main);
     }
 }
