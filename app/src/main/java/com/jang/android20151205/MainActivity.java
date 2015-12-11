e2b6722bd49b06824aaa6ebfa5a24aff4061af3c
janghe11 <jang0913@gmail.com>
2015-12-12 02:18:44 +0900

    20151212 #2 Life Cycle & Layout

--- app/src/main/java/com/jang/android20151205/MainActivity.java
+++ app/src/main/java/com/jang/android20151205/MainActivity.java
@@ -3,7 +3,10 @@ package com.jang.android20151205;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -14,5 +17,29 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         // 화면에 출력될 View 선택
         setContentView(R.layout.activity_main);
+
+
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        makeView();
+    }
+
+    private void makeView(){
+        Button btn = (Button)findViewById(R.id.btnMainActivityLogin);
+        btn.setOnClickListener(click);
+    }
+
+    View.OnClickListener click = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            int id = v.getId();
+
+            if(id == R.id.btnMainActivityLogin){
+                Log.d("20151212", "login clicked");
+            }
+        }
+    };
 }
